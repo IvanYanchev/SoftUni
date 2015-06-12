84f8d7028efae1f86902b09fa880a4f8daa41e8c
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-06-12 10:35:13 +0300

    Initial commit

---
+++ Basic Level/Programming Basics/05. Operators, Expressions, Statements/Homework/Point/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class PointInsideCircleAndOutsideRectangle
+{
+    static void Main()
+    {
+        double circleRadius = 1.5;
+        double circleCenterX = 1;
+        double circleCenterY = 1;
+        double rectangleTop = 1;
+        double rectangleLeft = -1;
+        double rectangleWidth = 6;
+        double rectangleHeight = 2;
+
+        Console.Write("Enter coordinate X: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("Enter coordinate Y: ");
+        double y = double.Parse(Console.ReadLine());
+        double dist = Math.Sqrt((x - circleCenterX) * (x - circleCenterX) + (y - circleCenterY) * (y - circleCenterY));
+        bool isInsideTheCircle = dist <= circleRadius;
+
+        bool isInsideTheRecatngle =
+            x >= rectangleLeft &&
+            x <= rectangleLeft + rectangleWidth &&
+            y >= rectangleTop - rectangleHeight &&
+            y <= rectangleTop;
+
+        bool result = isInsideTheCircle && !isInsideTheRecatngle;
+
+        Console.WriteLine("Is the point within the circle K({{{0}, {1}}}, {2}) and out of the rectangle R(top={3}, left={4}, width={5}, height={6}): {7}", circleCenterX, circleCenterY, circleRadius, rectangleTop, rectangleLeft, rectangleWidth, rectangleHeight, result);
+    }
+}
