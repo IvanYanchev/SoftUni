048455c1be2af90befd944e89742b64a337ebb6c
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-06-12 10:19:09 +0300

    Initial commit

---
+++ Basic Level/Programming Basics/05. Operators, Expressions, Statements/5. Operators-Expressions-and-Statements-Homework/PointInACircle/PointInACircle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class PointInACircle
+{
+    static void Main()
+    {
+        int circleRadius = 2;
+        Console.WriteLine("Enter coordinates to check if the point is inside the circle K({{0, 0}}, {0})", circleRadius);
+        Console.Write("X = ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("Y = ");
+        double y = double.Parse(Console.ReadLine());
+        double distToCenter = Math.Sqrt(x * x + y * y);
+        Console.WriteLine("Result : {0}", distToCenter <= circleRadius);
+    }
+}
