4229bb63bb28c71582ffbaee408b023ba4635213
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-06-12 10:18:50 +0300

    Initial commit

---
+++ Basic Level/Programming Basics/05. Operators, Expressions, Statements/5. Operators-Expressions-and-Statements-Homework/OddOrEvenIntegers/OddOrEvenIntegers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class OddOrEvenIntegers
+{
+    static void Main()
+    {
+        int[] numbers = new int[5];
+        numbers[0] = 3;
+        numbers[1] = 2;
+        numbers[2] = -2;
+        numbers[3] = -1;
+        numbers[4] = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            bool isOdd = (numbers[i] % 2) != 0;
+            Console.WriteLine(isOdd);
+        }
+    }
+}
