71ed1c2be386aacb40dbb7c50787f4eada946833
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-10-23 14:52:43 +0300

    Java-Collections-Homework update

--- 01 Fundamental Level/03 Java Fundamentals/4. Java-Collections/Java-Collections-Basics-Homework/src/_13_FilterArray.java
+++ 01 Fundamental Level/03 Java Fundamentals/4. Java-Collections/Java-Collections-Basics-Homework/src/_13_FilterArray.java
@@ -1,11 +1,17 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class _13_FilterArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String[] words = scanner.nextLine().split(" ");
+
+        Arrays.stream(words).filter(s -> s.length() > 3).forEach(System.out::println);
+
+        //List<String> filteredWords = Arrays.stream(words).filter(s -> s.length() > 3).collect(Collectors.toList());
+
     }
 }
--- 01 Fundamental Level/03 Java Fundamentals/4. Java-Collections/Java-Collections-Basics-Homework/src/_14_SortArrayWithStreamAPI.java
+++ 01 Fundamental Level/03 Java Fundamentals/4. Java-Collections/Java-Collections-Basics-Homework/src/_14_SortArrayWithStreamAPI.java
@@ -1,24 +1,30 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class _14_SortArrayWithStreamAPI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
-        String order = scanner.nextLine();
-
-        int[] numbers = new int[n];
-
         String[] elementsAsString = scanner.nextLine().split(" ");
 
+        String order = scanner.nextLine();
+
+        List<Integer> list = new ArrayList<Integer>();
+
         for (int i = 0; i < elementsAsString.length; i++) {
-            numbers[i] = Integer.parseInt(elementsAsString[i]);
+            list.add(Integer.parseInt(elementsAsString[i]));
         }
 
-        int[] sortedNumbers =  Arrays.stream(numbers).sorted().toArray();
+        switch (order) {
+            case "Ascending":
+                Collections.sort(list);
+                break;
+            case "Descending":
+                Collections.sort(list);
+                Collections.reverse(list);
+                break;
+        }
+
+        System.out.println(list);
     }
 }
