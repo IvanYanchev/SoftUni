32b8ec84027a5430a4fc0ede89eb3108b66a4e02
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-10-23 07:32:40 +0300

    Java-Streams-Homework update

--- 01 Fundamental Level/03 Java Fundamentals/3. Java-Streams/Java-Streams-Homework/src/_06_SaveCustomObjectInFile/Main.java
+++ 01 Fundamental Level/03 Java Fundamentals/3. Java-Streams/Java-Streams-Homework/src/_06_SaveCustomObjectInFile/Main.java
@@ -1,9 +1,36 @@
 package _06_SaveCustomObjectInFile;
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (ObjectOutputStream oos = new ObjectOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream("resources/course.save"))
+        )) {
+            Course course = new Course("Java Fundamentals", 300);
+
+            oos.writeObject(course);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(
+                new BufferedInputStream(
+                        new FileInputStream("resources/course.save"))
+        )) {
+            Course newCourse = (Course) ois.readObject();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- 01 Fundamental Level/03 Java Fundamentals/3. Java-Streams/Java-Streams-Homework/src/_07_CreateZipArchive/Main.java
+++ 01 Fundamental Level/03 Java Fundamentals/3. Java-Streams/Java-Streams-Homework/src/_07_CreateZipArchive/Main.java
@@ -1,9 +1,42 @@
 package _07_CreateZipArchive;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> filesForArchive = new ArrayList<String>() {{
+            add("resources/words.txt");
+            add("resources/count-chars.txt");
+            add("resources/lines.txt");
+        }};
+
+        try (ZipOutputStream zos = new ZipOutputStream(
+                new FileOutputStream("resources/text-files.zip")
+        )) {
+            for (String filename : filesForArchive) {
+                try (BufferedInputStream bis = new BufferedInputStream(
+                        new FileInputStream(filename)
+                )) {
+                    int bytes;
+                    
+                    zos.putNextEntry(new ZipEntry(filename));
+
+                    while ((bytes = bis.read()) != -1) {
+                        zos.write(bytes);
+                    }
+
+                    zos.closeEntry();
+                }
+            }
+
+        } catch (FileNotFoundException fnfExeption) {
+            fnfExeption.printStackTrace();
+        } catch (IOException ioExeption) {
+            ioExeption.printStackTrace();
+        }
     }
 }
