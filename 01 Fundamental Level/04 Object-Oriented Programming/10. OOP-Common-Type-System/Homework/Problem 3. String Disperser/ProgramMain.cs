e19b7c565c1f5765e4c161aceb18e66c0af6b295
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-12-16 20:50:22 +0200

    OOP-Common-Type-System-Homework

---
+++ 01 Fundamental Level/04 Object-Oriented Programming/10. OOP-Common-Type-System/Homework/Problem 3. String Disperser/ProgramMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problem_3.String_Disperser
+{
+    class ProgramMain
+    {
+        static void Main()
+        {
+            StringDisperser stringDisperser = new StringDisperser("gosho", "pesho", "tanio");
+            foreach (var ch in stringDisperser)
+            {
+                Console.Write(ch + " ");
+            }
+
+        }
+    }
+}
