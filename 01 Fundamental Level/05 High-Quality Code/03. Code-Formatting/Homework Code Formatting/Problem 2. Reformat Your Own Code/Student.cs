6b58a6c72ee1f9d5ca1fc339f10f736c08c51a63
Ivan Yanchev <ivan.yan4ev@gmail.com>
2016-01-07 20:18:04 +0200

    Code Formatting homework upload

---
+++ 01 Fundamental Level/05 High-Quality Code/03. Code-Formatting/Homework Code Formatting/Problem 2. Reformat Your Own Code/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem_4.Student_Class
+{
+    public delegate void OnPropertyChangeEventHandler
+        (Student student,
+        PropertyChangedEventArgs args);
+
+    public class Student
+    {
+        private string _name; private int _age;
+        public event OnPropertyChangeEventHandler
+            PropertyChanged;
+
+        public Student
+            (string name,
+            int age)
+        {this.Name = name;this.Age = age;
+        }
+
+        public string Name{get{return this._name;}
+
+            set{if (this.PropertyChanged != null) {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Name", this._name, value));
+                } this._name = value;}}
+
+        public int Age{get{return this._age;}
+
+            set{if (this.PropertyChanged != null) this.PropertyChanged(this, new PropertyChangedEventArgs("Age", this._age, value));
+                this._age = value;
+            }
+        }
+    }
+}
